b05f1cc680410ce6e19949eeb0fd3b12da058f8f
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-07 10:21:26 -0400

    Finish assignment 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  let result = ((far - 32) * 5) / 9;
+  result = +result.toFixed(1);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let result = (cel * 9) / 5 + 32;
+  result = +result.toFixed(1);
+
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
