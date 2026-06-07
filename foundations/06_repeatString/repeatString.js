1a733740ac3325c64d72bc226f00f941d144caf3
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-06 23:01:35 -0400

    Finish assignment 06-repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num >= 0) {
+    let strReturn = "";
+    for (let i = 0; i < num; i++) {
+      strReturn += str;
+    }
+    return strReturn;
+  } else {
+    return (strReturn = "ERROR");
+  }
 };
 
 // Do not edit below this line
