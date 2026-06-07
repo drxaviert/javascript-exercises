ff4c3f92392ee0f4124d4242f941235a01674649
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-07 09:58:37 -0400

    Finish assignment 09_SumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let arr = [];
+  let firstNum, secondNum;
 
+  if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    if (a <= b) {
+      firstNum = a;
+      secondNum = b;
+    } else {
+      firstNum = b;
+      secondNum = a;
+    }
+
+    for (let i = firstNum; i <= secondNum; i++) {
+      arr.push(i);
+    }
+
+    return arr.reduce((previous, current) => previous + current);
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
