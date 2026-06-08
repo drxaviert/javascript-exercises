419befcf8cac1ab66c144d5abba5a968103640ac
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-07 21:44:18 -0400

    Finnish assignment 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  for (let i = 0; i < num.length; i++) {
+    for (let j = arr.length - 1; j >= 0; j--) {
+      if (arr[j] === num[i]) {
+        arr.splice(j, 1);
+      }
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
