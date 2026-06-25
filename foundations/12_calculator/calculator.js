78da74b9ba9d32080db41d333747bf12f885b4b9
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-24 20:59:02 -0400

    Finish assignment 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((prev, val) => prev + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, val) => prev * val, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let arr = [];
+
+  for (let i = 1; i <= a; i++) {
+    arr.push(i);
+  }
+
+  return arr.reduce((prev, val) => prev * val, 1);
 };
 
 // Do not edit below this line
@@ -29,5 +35,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
