239215f6dc146936f6812066ec8239c2ad7b204c
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-25 17:35:37 -0400

    Finish assignment 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((prev, current) => {
+    let prevLatestYear, currentLatestYear;
 
+    if ("yearOfDeath" in prev) {
+      prevLatestYear = prev["yearOfDeath"];
+    } else {
+      prevLatestYear = new Date().getFullYear();
+    }
+    if ("yearOfDeath" in current) {
+      currentLatestYear = current["yearOfDeath"];
+    } else {
+      currentLatestYear = new Date().getFullYear();
+    }
+
+    let prevAge = prevLatestYear - prev.yearOfBirth;
+    let currentAge = currentLatestYear - current.yearOfBirth;
+
+    if (prevAge >= currentAge) {
+      result = prev;
+      return prev;
+    } else {
+      result = current;
+      return current;
+    }
+  });
 };
 
 // Do not edit below this line
