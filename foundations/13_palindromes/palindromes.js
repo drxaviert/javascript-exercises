22b804005095f7e1df606b5e0a2d850bb90fb1e4
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-26 15:25:23 -0400

    Finish assignment 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (strArg) {
+  let strArr = strArg
+    .split("")
+    .map((letter) => letter.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""))
+    .filter((letter) => letter != "");
 
+  let strFirstHalf = strArr.slice(0, Math.ceil(strArr.length / 2)).join("");
+  let strSecondHalf = strArr
+    .slice(Math.floor(strArr.length / 2))
+    .reverse()
+    .join("");
+
+  if (strFirstHalf === strSecondHalf) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
